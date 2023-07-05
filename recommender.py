cfca5469951fe93f19dfe81060c869ef70590ee0
Lorelei <132485802+LCerulean@users.noreply.github.com>
2023-07-05 13:11:22 -0400

    Update recommender.py additional search options to include

--- recommender.py
+++ recommender.py
@@ -1,7 +1,8 @@
 #should allow input of shop link
 #builds a recommended group of ball pythons for purchase based on:
   # age/maturity
-  # genes
+  # genes wanted (option for either all animals have said gene, or just want gene somewhere in the package)
+  # genes NOT wanted
   # gene count
   # snake count
   # sex -can be split male/female with specific amounts or ratios, or just all
