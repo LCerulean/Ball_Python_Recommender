658f231edd7deb129107880962ec28d4b6052feb
Lorelei <132485802+LCerulean@users.noreply.github.com>
2023-07-05 13:03:18 -0400

    Create recommender.py

---
+++ recommender.py
@@ -0,0 +1,9 @@
+#should allow input of shop link
+#builds a recommended group of ball pythons for purchase based on:
+  # age/maturity
+  # genes
+  # gene count
+  # snake count
+  # sex -can be split male/female with specific amounts or ratios, or just all
+  # individual price or budget
+  # take into account shipping price and store discounts
