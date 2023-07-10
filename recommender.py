c2335484c71a492efb840bd6199f0bf330f5a524
ObsessivelyBlue <118937230+ObsessivelyBlue@users.noreply.github.com>
2023-07-10 14:37:11 -0400

    Code format_shop_stock

--- recommender.py
+++ recommender.py
@@ -1,6 +1,4 @@
 import json
-import urllib
-import requests
 
 #should allow input of shop link
 #builds a recommended group of ball pythons for purchase based on:
@@ -17,22 +15,49 @@ import requests
 snakes = []
 
 #pulls data from web, converts to desired format using Python class, writes data to the shop_data file
-def download_snakes():
-  #asks user input for link
-  # url = input("What is the url for the shop?\n")
-  url = 'https://www.morphmarket.com/stores/crescentserpents/'
-  #checks if can connect to link, returns error if not
-  #needs to find the line that reads as:
-    #<a class="btn btn-sold btn-primary btn-ok" href="/stores/crescentserpents/export?format=json">JSON</a>
-    #downloads the json file into current directory folder
-  # url_html = urllib.request.urlopen(url).read()
-  url_html = requests.get(url).text()
-  print(url_html)
-
+def format_shop_stock():
   #convert to read and write to shop_data file, each snake on it's own line
   with open('animals.json') as raw_data:
     shop_data = json.load(raw_data) #type is list, indicies are dictionaries
     # print(shop_data)
+    for snake_data in shop_data:
+      if snake_data["State"] == "For Sale":
+        snake_info = {'Title*':'', 'Animal_Id*':'', 'Maturity*':'', 'Price':'', 'Sex':'', 'Traits':[], 'Prey_State':'', 'Prey_Food':''}
+        
+        #converting traits string value to list of traits
+        for key in snake_info:
+          if key == "Traits":
+            traits_split = snake_data[key].split(" ")
+            traits_list = []
+            trait_count = len(traits_split)
+            count = 0
+            while count < trait_count:
+              #checking if trait is two words, if so joining into single string before appending list
+              ## NOT joining hets or percents with traits, leaving seperate for search
+              if traits_split[count] == "black" or traits_split[count] == "yellow" or traits_split[count] == "lavender" or traits_split[count] == "pet":
+                trait = traits_split[count] + " " + traits_split[count+1]
+                traits_list.append(trait)
+                #skipping 2nd half of two word trait in next loop through traits_split
+                count += 1
+                if count >= len(traits_split):
+                  break
+              else:
+                traits_list.append(traits_split[count])
+              count += 1
+
+            snake_info["Traits"] = traits_list
+
+
+
+          else:
+          #copying relevant info from json file for each snake and putting it in list
+            snake_info[key] = snake_data[key]
+        
+        
+        snakes.append(snake_info)
+      
+  for snake in snakes:
+    print(f"{snake['Traits']}\n")
     
 
 #takes the snake data and uses Python class to create instances for each one in the shop, then adds to snakes list
@@ -76,4 +101,4 @@ def bag_of_snakes(snakes, order):
       # -goes through the price and gene stack to find best package deal
 
 
-download_snakes()
+format_shop_stock()
